39fdd09b930e261a742fbcc50d924776832eed3d
thanutcha <thanutcha.ki@ku.th>
2020-03-05 12:53:52 +0700

    no message

--- src/refrigerator/Main.java
+++ src/refrigerator/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("profile.fxml"));
         primaryStage.setTitle("Refrigerator");
-        primaryStage.setScene(new Scene(root, 1024, 1024));
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 
